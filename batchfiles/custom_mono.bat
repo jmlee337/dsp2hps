09d571256e063f122dbcaf9348c6caf4955fa8a7
Jeremy Lee <jmlee337@gmail.com>
2017-08-29 00:14:47 -0700

    replace parameterized intermediate files in custom loop batchfiles
    not sure why it was like that
    #1

--- batchfiles/custom_mono.bat
+++ batchfiles/custom_mono.bat
@@ -1,6 +1,6 @@
 REM SET THE --loop_point PARAMETER IN SECONDS
 REM AVAILABLE PRECISION IS 56 / SAMPLE_RATE SECONDS
-REM DEFAULT 0.00175 SECONDS
+REM WITH DEFAULT SAMPLE_RATE, THAT'S 0.00175 SECONDS
 
 @echo off
 :loop
@@ -8,18 +8,13 @@ title Converting DSP
 tool\ffmpeg.exe -y -i %1 -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 LEFT.wav -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 RIGHT.wav
 
 set "filename=%1"
-set "left=left"
-set "right=right"
-set "dsp=.dsp"
-set "leftdspname=%filename%%left%%dsp%"
-set "rightdspname=%filename%%right%%dsp%"
-tool\DSPADPCM.exe -e LEFT.wav %leftdspname%
-tool\DSPADPCM.exe -e RIGHT.wav %rightdspname%
+tool\DSPADPCM.exe -e LEFT.wav LEFT.dsp
+tool\DSPADPCM.exe -e RIGHT.wav RIGHT.dsp
 
 title Writing HPS
 set "hps=.hps"
 set "fullname=%filename%%hps%"
-tool\dsp2hps.exe -l %leftdspname% -r %rightdspname% -o %fullname% --loop_point 17.161
+tool\dsp2hps.exe -l LEFT.dsp -r RIGHT.dsp -o %fullname% --loop_point 17.161
 shift
 if not "%~1"=="" goto loop
 
@@ -28,7 +23,7 @@ echo Will delete intermediate files
 pause
 del LEFT.wav
 del RIGHT.wav
-del %leftdspname%
-del %rightdspname%
+del LEFT.dsp
+del RIGHT.dsp
 del LEFT.txt
 del RIGHT.txt
