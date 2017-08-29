7d3ca120ec8b4def0216f1345513b4ba30718779
Jeremy Lee <jmlee337@gmail.com>
2017-08-29 00:05:34 -0700

    commit the batch files to the repo
    will follow up with fix for #1

---
+++ batchfiles/custom_mono.bat
@@ -0,0 +1,34 @@
+REM SET THE --loop_point PARAMETER IN SECONDS
+REM AVAILABLE PRECISION IS 56 / SAMPLE_RATE SECONDS
+REM DEFAULT 0.00175 SECONDS
+
+@echo off
+:loop
+title Converting DSP
+tool\ffmpeg.exe -y -i %1 -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 LEFT.wav -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 RIGHT.wav
+
+set "filename=%1"
+set "left=left"
+set "right=right"
+set "dsp=.dsp"
+set "leftdspname=%filename%%left%%dsp%"
+set "rightdspname=%filename%%right%%dsp%"
+tool\DSPADPCM.exe -e LEFT.wav %leftdspname%
+tool\DSPADPCM.exe -e RIGHT.wav %rightdspname%
+
+title Writing HPS
+set "hps=.hps"
+set "fullname=%filename%%hps%"
+tool\dsp2hps.exe -l %leftdspname% -r %rightdspname% -o %fullname% --loop_point 17.161
+shift
+if not "%~1"=="" goto loop
+
+title Done!
+echo Will delete intermediate files
+pause
+del LEFT.wav
+del RIGHT.wav
+del %leftdspname%
+del %rightdspname%
+del LEFT.txt
+del RIGHT.txt
---
+++ batchfiles/normal_mono.bat
@@ -0,0 +1,21 @@
+@echo off
+:loop
+title Converting DSP
+tool\ffmpeg.exe -y -i %1 -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 LEFT.wav -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 RIGHT.wav
+tool\DSPADPCM.exe -e LEFT.wav LEFT.dsp
+tool\DSPADPCM.exe -e RIGHT.wav RIGHT.dsp
+title Writing HPS
+set "filename=%1"
+set "extension=.hps"
+set "fullname=%filename%%extension%"
+tool\dsp2hps.exe -l LEFT.dsp -r RIGHT.dsp -o %fullname%
+shift
+if not "%~1"=="" goto loop
+echo Will delete intermediate files
+pause
+del LEFT.wav
+del RIGHT.wav
+del LEFT.dsp
+del RIGHT.dsp
+del LEFT.txt
+del RIGHT.txt
