b827c0da755319452543292b7b1b099515ff195f
Jeremy Lee <jmlee337@gmail.com>
2017-08-30 21:18:15 -0700

    preserve filenames to allow filename looping, but still delete as we go
    should be good for #1 now

--- batchfiles/custom_mono.bat
+++ batchfiles/custom_mono.bat
@@ -8,22 +8,27 @@ title Converting DSP
 tool\ffmpeg.exe -y -i %1 -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 LEFT.wav -ar 32000 -acodec pcm_s16le -map_channel 0.0.0 RIGHT.wav
 
 set "filename=%1"
-tool\DSPADPCM.exe -e LEFT.wav LEFT.dsp
-tool\DSPADPCM.exe -e RIGHT.wav RIGHT.dsp
+set "left=left"
+set "right=right"
+set "dsp=.dsp"
+set "leftdspname=%filename%%left%%dsp%"
+set "rightdspname=%filename%%right%%dsp%"
+tool\DSPADPCM.exe -e LEFT.wav %leftdspname%
+tool\DSPADPCM.exe -e RIGHT.wav %rightdspname%
 
 title Writing HPS
 set "hps=.hps"
 set "fullname=%filename%%hps%"
-tool\dsp2hps.exe -l LEFT.dsp -r RIGHT.dsp -o %fullname% --loop_point 17.161
+tool\dsp2hps.exe -l %leftdspname% -r %rightdspname% -o %fullname% --loop_point 17.161
+del %leftdspname%
+del %rightdspname%
 shift
 if not "%~1"=="" goto loop
 
 title Done!
-echo Will delete intermediate files
+echo Will delete remaining intermediate files
 pause
 del LEFT.wav
 del RIGHT.wav
-del LEFT.dsp
-del RIGHT.dsp
 del LEFT.txt
 del RIGHT.txt
